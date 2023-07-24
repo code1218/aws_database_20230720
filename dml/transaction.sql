da3d61c1f18991fd58663a21a32d4db5772b11c5
devgiants75 <devgiants75@naver.com>
2023-07-24 10:03:51 +0900

    프로시저, 트랜잭션

---
+++ dml/transaction.sql
@@ -0,0 +1,27 @@
+select @@autocommit;
+set autocommit = 0;	#commit off
+
+start transaction;
+
+savepoint insert_user;
+
+insert into user_tb
+values(0, 'ddd', '1234');
+
+select * from user_tb;
+
+savepoint update_user_password;
+
+update user_tb set password = '1111' where username = 'ddd';
+
+select * from user_tb;
+
+rollback to savepoint update_user_password;
+
+rollback to savepoint insert_user;
+
+rollback;
+
+commit;
+
+    
